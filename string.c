4b2086daeb3cc65aae0f434511f81bc1fe0648c2
sachins6408-creator <sachins6408@gmail.com>
2025-09-03 21:40:42 +0530

    string

---
+++ string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+char ch[100]="C";
+char s[100]="Language";
+char sen[100]="Welcome To C!!";
+
+printf("%s\n%s\n%s\n",ch,s,sen);
+    
+    return 0;
+}
