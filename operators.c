377c4f6fa16fb394f99d57a7c61afdf7c9197f64
sachins6408-creator <sachins6408@gmail.com>
2025-09-08 19:07:29 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y;
+
+  x=10;
+  y=4;
+  float a,b;
+a=4.0;
+b=2.0;
+   printf("%d %d\n",x+y,x-y);
+   printf("%.1f %.1f\n",a+b,a-b);
+   
+    
+    return 0;
+}
